945be590c3813c360fc4eb8d6158b40e1239695f
richardmaccaw <ricci6@gmail.com>
2018-11-07 14:40:57 +0000

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,21 @@
+class CouponsController < ApplicationController
+
+    def index
+        @coupons = Coupon.all
+    end
+
+    def new
+
+    end
+
+    def create
+        
+        Coupon.create(store: params[:coupon][:store], coupon_code: params[:coupon][:coupon_code])
+        redirect_to coupons_path
+    end
+
+    def show
+        @coupon = Coupon.find(params[:id])
+    end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.delete_all
+Coupon.create(store: 'London', coupon_code: 'XYD765')
+Coupon.create(store: 'Birmingham', coupon_code: 'DHJ387')
+Coupon.create(store: 'Brighton', coupon_code: 'DKEI732')
+Coupon.create(store: 'Wolverhampton', coupon_code: 'L3123LD')
+Coupon.create(store: 'Scunthorpe', coupon_code: 'ADK2')
+Coupon.create(store: 'Manchester', coupon_code: 'DAAD3')
+Coupon.create(store: 'Glasgow', coupon_code: 'ADFK234')
